2299e856e6a8b57b52b0a1dbaceb0caf0fc33fd3
Jaroslav Meloun <jaroslav.meloun@gmail.com>
2013-04-13 18:11:18 +0200

    handle multiframe images

--- model/symbol/SymbolImage.hx
+++ model/symbol/SymbolImage.hx
@@ -1,6 +1,7 @@
 package pug.model.symbol;
 
 import haxe.io.Bytes;
+import nme.geom.Point;
 import nme.geom.Rectangle;
 import nme.display.BitmapData;
 import nme.utils.ByteArray;
@@ -12,7 +13,9 @@ class SymbolImage extends Symbol
 {
     public static function parse( xml:Xml, l:Library, libData:LIB_DATA ):Symbol {
         var id:String = xml.get("id");
-        return new SymbolImage( id, libData.images.get( id ) );
+		var fw:Int = Std.parseInt( xml.get("frameWidth") );
+		var fh:Int = Std.parseInt( xml.get("frameHeight") );
+        return new SymbolImage( id, libData.images.get( id ), fw, fh );
     }
 	
 	private function BAtoBytes( ba:ByteArray ):Bytes {
@@ -23,17 +26,46 @@ class SymbolImage extends Symbol
 		return b;
 	}
 	
-    public var bitmapData:BitmapData;
+    public var frames:Array<BitmapData>;
+	public var frameHeight:Int;
+	public var frameWidth:Int;
 
-	public function new ( id:String, bmd:BitmapData ) 
-	{
+	public function new ( id:String, bmd:BitmapData, frameWidth:Int = 0, frameHeight:Int = 0 ) {
 		super( id );
 		
-        bitmapData = bmd;
-		size = new Rectangle( 0, 0, bitmapData.width, bitmapData.height );
+		if ( frameWidth == 0 || frameHeight == 0 ) {
+			frameWidth = bmd.width;
+			frameHeight = bmd.height;
+		}
+		
+		frames = [];
+		var frameCount:Int = Math.ceil( bmd.width / frameWidth );
+		var frame:BitmapData;
+		var r:Rectangle = new Rectangle( 0, 0, frameWidth, frameHeight );
+		var p:Point = new Point();
+		for ( i in 0...frameCount ) {
+			r.x = i * frameWidth;
+			r.width = Math.min( frameWidth, bmd.width - r.x );
+			frame = new BitmapData( frameWidth, frameHeight );
+			frame.copyPixels( bmd, r, p );
+			frames.push( frame );
+		}
+		
+		this.frameWidth = frameWidth;
+		this.frameHeight = frameHeight;
+		
+		size = new Rectangle( 0, 0, frameWidth, frameHeight );
 	}
 
 	private function getPNGBytes():Bytes {
+		var bitmapData:BitmapData = new BitmapData( frames.length * frameWidth, frameWidth );
+		var r:Rectangle = new Rectangle( 0, 0, frameWidth, frameHeight );
+		var p:Point = new Point();
+		for ( i in 0...frames.length ) {
+			p.x = i * frameWidth;
+			bitmapData.copyPixels( frames[i], r, p );
+		}
+		
         #if flash
 			var data:ByteArray = PNGEncoder.encode( bitmapData );
 			return BAtoBytes( data );
@@ -48,6 +80,8 @@ class SymbolImage extends Symbol
         var filename:String = id+".png";
 		xml.set( "id", id );
 		xml.set( "data", filename );
+		xml.set( "frameWidth", Std.string( frameWidth ) );
+		xml.set( "frameHeight", Std.string( frameHeight ) );
         export.xml = xml;
         export.files.push( {
             name: filename,
--- render/RenderImage.hx
+++ render/RenderImage.hx
@@ -11,17 +11,19 @@ import nme.display.Bitmap;
 class RenderImage extends Render
 {
 	public var image:SymbolImage;
+	public var bitmap:Bitmap;
 
 	public function new( effect:Effect, image:SymbolImage ) {
 		super( effect );
 		this.image = image;
-		var b:Bitmap;
-		addChild( b = new Bitmap( image.bitmapData ) );		
-        b.x = -b.width/2;
-        b.y = -b.height/2;
+		addChild( bitmap = new Bitmap() );
+		bitmap.bitmapData = image.frames[ 0 ];
+        bitmap.x = -image.frameWidth/2;
+        bitmap.y = -image.frameHeight/2;
 	}
 	
 	override public function render( frame:Int, applyTransforms:Bool = true ):Void {
+		bitmap.bitmapData = image.frames[ frame ];
 		super.render( frame, applyTransforms );
 		
 	}
