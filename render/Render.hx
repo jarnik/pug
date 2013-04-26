c83fe7903389b5210a346942573f7ddd28a9abed
Jaroslav Meloun <jaroslav.meloun@gmail.com>
2013-04-26 21:55:47 +0200

    onclicks

--- render/Render.hx
+++ render/Render.hx
@@ -22,6 +22,7 @@ import pug.model.symbol.Symbol;
 import pug.model.value.ValueAngle;
 import pug.model.value.ValueScale;
 import pug.model.value.ValueString;
+import nme.events.MouseEvent;
 
 /**
  * ...
@@ -105,11 +106,12 @@ class Render extends Sprite
 		}
 	}
 	
-	public function play( loop:Bool = false, fps:Float = 30 ):Void {
+	public function play( loop:Bool = false, fps:Float = 30, state:String = null, onFinishedCallback:Dynamic = null ):Void {
 		if ( player == null ) {
 			player = new Player();
 			player.onSetFrame.bind( onSetFrame );
 		}
+		onSetFrame( 0 );
 		player.play( loop, fps );
 	}
 	
@@ -137,4 +139,17 @@ class Render extends Sprite
 		var alpha:Array<Dynamic> = g.paramAlpha.getValues( frame );
         this.alpha = alpha[ 0 ];
     }
+	
+	public function onClick( _callback:Dynamic = null ):Void {
+        buttonMode = true;
+        mouseChildren = false;
+        addEventListener( MouseEvent.CLICK, _callback );
+    }
+
+    public function onEvents( events:Array<String>, _callback:Dynamic = null ):Void {
+        buttonMode = true;
+        mouseChildren = false;
+        for ( e in events )
+            addEventListener( e, _callback );
+    } 
 }
