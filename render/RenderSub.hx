18afd05ea1434b957ce9d07825c7a4ce04147509
Jaroslav Meloun <jaroslav.meloun@gmail.com>
2013-06-07 16:40:13 +0200

    update subrender

--- render/RenderSub.hx
+++ render/RenderSub.hx
@@ -16,10 +16,22 @@ class RenderSub extends Render
 {
 	private var sub:ISymbolSub;
 	private var s:DisplayObject;
+	private var cached_path:String;
 
 	public function new( effect:Effect, sub:ISymbolSub ) {
 		super( effect );
 		this.sub = sub;
+		
+		updateSub();
+	}
+	
+	private function updateSub():Void {
+		if ( (sub.source + "_" + sub.path) == cached_path)
+			return;
+		
+		if ( s != null )
+			removeChild( s );
+		cached_path = sub.source + "_" + sub.path;
 		
 		var symbol:Symbol = Library.lib.get( sub.source );
 		if ( symbol != null ) {
@@ -36,6 +48,11 @@ class RenderSub extends Render
 				}
 			}
 		}
+	}
+	
+	public override function render( frame:Int, applyTransforms:Bool = true ):Void {
+		super.render( frame, applyTransforms );
+		updateSub();
 	}
 	
 }
